e0928033700b934e7a6bb5923f77c8e071702f15
ChallengerJolt <33758094+ChallengerJolt@users.noreply.github.com>
2018-03-10 23:57:20 -0500

    Update main v0.0.1.cpp

--- main v0.0.1.cpp
+++ main v0.0.1.cpp
@@ -6,58 +6,75 @@
 #include <string>
 using namespace std;
 
-int begining(string playerText, int &roomNumber)
+int Room0(string &playerText, int &roomNumber)
 {
-	cout << "Welcome to the Maze \nType 'GameStart' to enter\nPlease note this game does not use spaces for input\n";
-	cin >> playerText;
-	if (playerText == "Quit", "quit")
+	while (roomNumber == 0)
 	{
-		return 0;
+		cout << "Welcome to the Maze \nType 'GameStart' to enter\nPlease note this game does not use spaces for input\n";
+		cin >> playerText;
+		if (playerText == "Quit" || playerText == "quit")
+		{
+			return 0;
+		}
+		else if (playerText == "Gamestart" || playerText == "GameStart")
+		{
+			roomNumber++;
+			return 0;
+		}
+		else
+		{
+			cout << "Unknown Action\n Please Try Again\n";
+		}
 	}
-	else if (playerText == "Gamestart", "GameStart")
-	{
-		return (roomNumber++);
-	}
-	else if (playerText != "Quit", "quit", "Gamestart", "GameStart")
-	{
-		cout << "Unknown Action/n Please Try Again/n";
-	}
-	cin >> playerText;
 }
 
 
-int main(string playerText, int &roomNumber)
+int main()
 {
 	// Variables
 	string playerText = "A";
 	int roomNumber = 0;
+	bool showIntro = true;
 	// Game start
 
-	cin >> playerText;
-	while (playerText != "Quit")
+
+	while (playerText != "Quit") //Loop start
 	{
+		if (showIntro == true)
+		{
+			Room0(playerText, roomNumber);
+			showIntro = false;
+		}
 		cin >> playerText;
-		cin >> playerText;
-		if (playerText == "GameStart")
+		if (roomNumber == 1)
 		{
 			cout << "You awake to see you are lying in a small pool of water, your memories are fuzzy you seem to\n remember that you were looking for something but nothing else.\n";
+			while (roomNumber == 1)
+			{
+
+				cin >> playerText;
+				if (playerText == "Lookaround" || playerText == "LookAround" || playerText == "lookaround" || playerText == "lookAround")
+				{
+					cout << "You see an empty room the dust swirling in the air light seems to be shining from within the pool, you also notice a door on the other side of the room.\n";
+				}
+				else if (playerText == "Lookup" || playerText == "LookUp" || playerText == "lookup" || playerText == "lookUp")
+				{
+					cout << "You see a small hole about your width in the ceiling.\n";
+				}
+				else if (playerText == "OpenDoor" || playerText == "Opendoor" || playerText == "opendoor" || playerText == "opendoor")
+				{
+					cout << "The door swings open easily\n and you see another room with three more doors.\n";
+					roomNumber++;
+				}
+				else
+				{
+					cout << "Please try again.\n";
+				}
+			}
 		}
-		if (playerText != "GameStart")
-		{
-			cout << "Please spell it correctly.\n";
-		}
+
 	}
-	if (playerText == "Look_around", "look_around", "Look_Around", "look_Around", "Lookaround", "LookAround", "lookaround", "lookAround")
-	{
-		cout << "You see an empty room the dust swirling in the air light seems to be shining from within the pool, you also notice a door on the other side of the room.\n";
-	}
-	else
-	{
-		cout << "sanity check.\n";
-	}
-	if (playerText == "Look_up", "look_up", "Look_Up", "look_Up", "Lookup", "LookUp", "lookup", "lookUp")
-	{
-		cout << "You see a small hole about your width in the ceiling.\n";
-	}
+
+
+	return 0;
 }
-return 0;
